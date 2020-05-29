4d446ef69f30617761616a1eadd609b86ae26042
Gerald Siegert <gerald.siegert@outlook.com>
2020-05-29 23:36:43 +0200

    using embed for kicker tables

--- botCommands/sport.py
+++ botCommands/sport.py
@@ -10,8 +10,14 @@ class sportCommands(commands.Cog, name="Sport Commands"):
     @commands.command(name="kicker", help="Returns frequently used links to kicker.de")
     async def kicker_table(self, ctx):
         """Returns the kicker.de Bundesliga tables"""
-        linklist=("Tabelle Bundesliga: <https://www.kicker.de/1-bundesliga/tabelle>\n"
-                  "Tabelle 2. Bundesliga: <https://www.kicker.de/2-bundesliga/tabelle>\n"
-                  "Tabelle 3. Liga: <https://www.kicker.de/3-liga/tabelle>\n"
-                  "Tabelle AT-Bundesliga: <https://www.kicker.de/tipp3-bundesliga/tabelle>")
-        await ctx.send(linklist)
+        embed = discord.Embed(title='Kicker.de-Tabellenlinks')
+        embed.add_field(name="Bundesliga", value="https://www.kicker.de/1-bundesliga/tabelle")
+        embed.add_field(name="2. Bundesliga", value="https://www.kicker.de/2-bundesliga/tabelle")
+        embed.add_field(name="3. Liga", value="https://www.kicker.de/3-liga/tabelle")
+        embed.add_field(name="AT-Bundesliga", value="https://www.kicker.de/tipp3-bundesliga/tabelle")
+
+        #linklist=("Tabelle Bundesliga: <https://www.kicker.de/1-bundesliga/tabelle>\n"
+        #          "Tabelle 2. Bundesliga: <https://www.kicker.de/2-bundesliga/tabelle>\n"
+        #          "Tabelle 3. Liga: <https://www.kicker.de/3-liga/tabelle>\n"
+        #          "Tabelle AT-Bundesliga: <https://www.kicker.de/tipp3-bundesliga/tabelle>")
+        await ctx.send(embed=embed)
