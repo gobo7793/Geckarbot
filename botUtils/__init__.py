d689c792b96530c64bdfba1c3b2e486946e30fd4
Gerald Siegert <gerald.siegert@outlook.com>
2020-05-29 22:55:27 +0200

    added decorator for channel-only cmds

--- botUtils/__init__.py
+++ botUtils/__init__.py
@@ -1,4 +1,5 @@
 import os
+from discord.ext import commands
 from discord.ext.commands.bot import Bot
 from config import config
 
@@ -7,3 +8,8 @@ async def write_debug_channel(bot:Bot, message):
     debug_chan = bot.get_channel(config.DEBUG_CHAN_ID)
     if(debug_chan != None):
         await debug_chan.send(message)
+
+def in_channel(channel_id):
+    def predicate(ctx):
+        return ctx.message.channel.id == channel_id
+    return commands.check(predicate)
