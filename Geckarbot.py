6f06bcf922b08820eb68c9b763d573a24965d104
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-02 22:40:34 +0200

    changed server in welcome message

--- Geckarbot.py
+++ Geckarbot.py
@@ -91,7 +91,7 @@ if not Config().DEBUG_MODE:
 async def on_member_join(member):
     """Write new users a short dm"""
     await member.create_dm()
-    await member.dm_channel.send(f"Hi {member.display_name}, Willkommen auf dem #storm-Discord-Server!\n"
+    await member.dm_channel.send(f"Hi {member.display_name}, Willkommen auf dem Communityserver!\n"
                                  f"Schreibe am besten einem @mod, um die entsprechenden Rechte zu bekommen.")
 
 
