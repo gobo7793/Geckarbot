cc762049b75d64969509fb53395a21ee17ad85da
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-02 20:07:44 +0200

    fixed error in in_channel

--- botUtils/permChecks.py
+++ botUtils/permChecks.py
@@ -5,9 +5,10 @@ from discord.ext import commands
 def in_channel(channel_id):
     """Check if CMD can be used in the channel with given channel id"""
     def predicate(ctx):
-        return (isinstance(ctx.channel, discord.DMChannel)
-                    or isinstance(ctx.channel, discord.GroupChannel)
-                    or ctx.channel.id is channel_id)
+        is_dm = isinstance(ctx.channel, discord.DMChannel)
+        is_group = isinstance(ctx.channel, discord.GroupChannel)
+        is_id = ctx.channel.id == channel_id
+        return is_dm or is_group or is_id
     return commands.check(predicate)
 
 def check_full_access(user:discord.Member):
