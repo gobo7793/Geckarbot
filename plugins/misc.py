8573c2e510cfe1d90b519eee4410eed64e5993db
Christian <christian@emailgsm.de>
2020-06-03 00:17:25 +0200

    remade plugin structure

--- plugins/misc.py
+++ plugins/misc.py
@@ -138,7 +138,3 @@ class Plugin(commands.Cog, name="Funny/Misc Commands"):
         Config().dsc['state_end'] = datetime.strptime(dateStr,"%d.%m.%Y %H:%M")
         Config().write_config_file()
         await ctx.send("New state end date set.")
-
-
-def register(bot):
-    bot.add_cog(miscCommands(bot))
