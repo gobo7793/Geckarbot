89ef01ed32aae091e2734b53204abbf161035c81
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-07 15:15:23 +0200

    added log output to admin channel #40

--- botutils/utils.py
+++ botutils/utils.py
@@ -1,3 +1,6 @@
+import pytz
+import discord
+import datetime
 from discord.ext.commands.bot import Bot
 from conf import Config
 from threading import Thread, Lock
@@ -55,26 +58,6 @@ class AsyncTimer(Thread):
             self.cancelled = True
 
 
-async def write_debug_channel(bot: Bot, message):
-    """Writes the given message to the bot's debug channel"""
-    debug_chan = bot.get_channel(Config().DEBUG_CHAN_ID)
-    if debug_chan is not None:
-        await debug_chan.send(message)
-
-
-async def write_debug_channel_embed(bot: Bot, embed):
-    """Writes the given message to the bot's debug channel"""
-    debug_chan = bot.get_channel(Config().DEBUG_CHAN_ID)
-    if debug_chan is not None:
-        await debug_chan.send(embed=embed)
-
-async def write_admin_channel(bot: Bot, message):
-    """Writes the message to the admin channel"""
-    admin_chan = bot.get_channel(Config().ADMIN_CHAN_ID)
-    if admin_chan is not None:
-        await admin_chan.send()
-
-
 def get_best_username(user):
     """
     :param user: User (Member or User instance) that is to be identified
@@ -110,3 +93,49 @@ def clear_link(link):
     if link.endswith('>'):
         link = link[:-1]
     return link
+
+
+def convert_to_local_time(timestamp):
+    """
+    Converts the given timestamp from UTC to local time
+    :param datetime: The datetime instance of the timestamp
+    """
+    return timestamp.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+
+
+async def write_debug_channel(bot: Bot, message):
+    """Writes the given message or embed to the debug channel"""
+    debug_chan = bot.get_channel(Config().DEBUG_CHAN_ID)
+    if debug_chan is not None:
+        if isinstance(message, discord.Embed):
+            await debug_chan.send(embed=message)
+        else:
+            await debug_chan.send(message)
+
+
+async def write_admin_channel(bot: Bot, message):
+    """Writes the given message or embed to the admin channel"""
+    admin_chan = bot.get_channel(Config().ADMIN_CHAN_ID)
+    if admin_chan is not None:
+        if isinstance(message, discord.Embed):
+            await admin_chan.send(embed=message)
+        else:
+            await admin_chan.send(message)
+
+
+async def log_to_admin_channel(context):
+    """
+    Logs the context to admin channel with following content:
+    Author name, Timestamp, Channel name, Message
+    :param bot: The bot
+    :param context: The context to log to the admin channel
+    """
+    author_field = "{}#{} in {}".format(context.author.name, context.author.discriminator, context.channel.name)
+    timestamp = convert_to_local_time(context.message.created_at)
+
+    embed = discord.Embed(title=context.message.clean_content)
+    embed.set_author(name=author_field)
+    embed.timestamp = timestamp
+    embed.description = context.message.jump_url
+
+    await write_admin_channel(context.bot, embed)
