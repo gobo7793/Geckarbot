4960e3fa961055f13dce044aee17267759804696
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-08 20:48:51 +0200

    updated version

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ class Config(metaclass=_Singleton):
     # Basic bot info
     ######
 
-    VERSION = "1.1.0"
+    VERSION = "1.2.0"
     CONFIG_DIR = "config"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
