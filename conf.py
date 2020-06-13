ffbac1af15181bdef5b12bf6a3e878dabaa0528c
Fluggs <christian@emailgsm.de>
2020-06-13 12:30:59 +0200

    incremented version counter

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ class Config(metaclass=_Singleton):
     # Basic bot info
     ######
 
-    VERSION = "1.3.1"
+    VERSION = "1.3.2"
     CONFIG_DIR = "config"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
