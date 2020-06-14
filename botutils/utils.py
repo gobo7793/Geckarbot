ffc6562504a3db6b702b5af187c7191dd04f1069
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-14 22:57:15 +0200

    disabled admin log in debug mode

--- botutils/utils.py
+++ botutils/utils.py
@@ -126,16 +126,16 @@ async def write_admin_channel(bot: Bot, message):
 async def log_to_admin_channel(context):
     """
     Logs the context to admin channel with following content:
-    Author name, Timestamp, Channel name, Message
+    Author name, Timestamp, Channel name, Message.
+    Doesn't log if Config().DEBUG_MODE is True.
     :param context: The context to log to the admin channel
     """
-    #author_field = "{}#{} in {}".format(context.author.name, context.author.discriminator, context.channel.name)
+    if Config().DEBUG_MODE == True:
+        return
+
     timestamp = convert_to_local_time(context.message.created_at).strftime('%d.%m.%Y, %H:%M')
 
     embed = discord.Embed(title=context.message.clean_content)
-    #embed.set_author(name=author_field)
-    #embed.timestamp = timestamp
-    #embed.description = context.message.jump_url
     embed.add_field(name="User", value=context.author.mention)
     embed.add_field(name="Channel", value=context.channel.mention)
     embed.add_field(name="Timestamp", value=timestamp)
