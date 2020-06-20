196e9c1afe3ec5ae3b93570c47261f7617035744
Gerald Siegert <gerald.siegert@outlook.com>
2020-06-20 21:29:12 +0200

    using bots reaction listener

--- coreplugins/roles.py
+++ coreplugins/roles.py
@@ -1,11 +1,12 @@
 from copy import deepcopy
 
 import discord
+import emoji
 from discord.ext import commands
 
 from Geckarbot import BasePlugin
 from conf import Config
-from botutils import utils, permChecks
+from botutils import utils, permChecks, reactions
 
 
 class RoleManagement:
@@ -34,7 +35,7 @@ class RoleManagement:
         await member.remove_roles(role)
 
     @staticmethod
-    async def add_server_role(guild: discord.Guild, name, color: discord.Color = None, mentionable = True):
+    async def add_server_role(guild: discord.Guild, name, color: discord.Color = None, mentionable=True):
         """
         Creates a roll on the server
         :param guild: the server guild
@@ -64,7 +65,7 @@ class ReactionEventData:
     Tuple for easier reaction event handling.
     """
 
-    def __init__(self, user, channel, message, emoji, member = None):
+    def __init__(self, user, channel, message, emoji, member=None):
         self.user = user
         self.channel = channel
         self.message = message
@@ -103,14 +104,19 @@ class Plugin(BasePlugin, name="Role Management"):
     def __init__(self, bot):
         super().__init__(bot)
         bot.register(self)
-        
-        @bot.listen()
-        async def on_raw_reaction_add(payload):
-            await self.update_reaction_based_user_role(payload, True)
 
         @bot.listen()
-        async def on_raw_reaction_remove(payload):
-            await self.update_reaction_based_user_role(payload, False)
+        async def on_ready():
+            if self.has_init_msg_set():
+                bot.reaction_listener.register(await self.get_init_msg(), self.update_reaction_based_user_role)
+
+        # @bot.listen()
+        # async def on_raw_reaction_add(payload):
+        #     await self.update_reaction_based_user_role(payload, True)
+        #
+        # @bot.listen()
+        # async def on_raw_reaction_remove(payload):
+        #     await self.update_reaction_based_user_role(payload, False)
 
     def default_config(self):
         return {
@@ -118,13 +124,25 @@ class Plugin(BasePlugin, name="Role Management"):
                 'channel_id': Config().CHAN_IDS['announcements'],
                 'message_id': 0,
                 'content': ""
-                },
+            },
             'roles': {}
-            }
+        }
 
     def rc(self):
         """Returns the roles config"""
         return Config().get(self)['roles']
+
+    def has_init_msg_set(self):
+        return (Config().get(self)['message']['channel_id'] != 0
+                and Config().get(self)['message']['message_id'] != 0)
+
+    async def get_init_msg(self):
+        """Returns the role management init message or None if not set"""
+        if self.has_init_msg_set():
+            channel = self.bot.get_channel(Config().get(self)['message']['channel_id'])
+            return await channel.fetch_message(Config().get(self)['message']['message_id'])
+        else:
+            return None
 
     async def create_message_text(self, server_roles, ctx):
         """
@@ -134,7 +152,7 @@ class Plugin(BasePlugin, name="Role Management"):
         :param ctx: The context of the used command to create the new message
         """
         msg = "{}\n".format(Config().get(self)['message']['content'])
-        
+
         for rid in self.rc():
             role = discord.utils.get(server_roles, id=rid)
             emote_msg = ""
@@ -175,27 +193,24 @@ class Plugin(BasePlugin, name="Role Management"):
         removed_roles = deepcopy(copy_roles)
         for role_in_config in self.rc():
             if role_in_config in server_role_ids:
-                del(removed_roles[role_in_config])
+                del (removed_roles[role_in_config])
 
         for role_to_remove in removed_roles:
-            del(self.rc()[role_to_remove])
+            del (self.rc()[role_to_remove])
         roles_debug = self.rc()
 
         # update message
         message_text = await self.create_message_text(current_server_roles, ctx)
+        message = await self.get_init_msg().edit(content=message_text)
 
-        channel = self.bot.get_channel(Config().get(self)['message']['channel_id'])
-        try:
-            message = await channel.fetch_message(Config().get(self)['message']['message_id'])
-        except:
-            message = None
-
-        if message is None:
-            await utils.write_debug_channel(self.bot, Contig().lang(self, 'creating_init_msg'))
+        if message is not None:
+            await self.get_init_msg().edit(content=message_text)
+        else:
+            await utils.write_debug_channel(self.bot, Config().lang(self, 'creating_init_msg'))
+            channel = self.bot.get_channel(Config().get(self)['message']['channel_id'])
             message = await channel.send(message_text)
             Config().get(self)['message']['message_id'] = message.id
-        else:
-            await message.edit(content=message_text)
+            self.bot.reaction_listener.register(message, self.update_reaction_based_user_role)
 
         # remove reactions from old roles
         for removed_role in removed_roles:
@@ -216,41 +231,34 @@ class Plugin(BasePlugin, name="Role Management"):
 
         Config().save(self)
 
-    async def update_reaction_based_user_role(self, payload, role_add=None):
+    async def update_reaction_based_user_role(self, event):
         """
         Updates the user roles based on the reaction events.
-        :param payload: The RawReactionActionEvent data
-        :param role_add: For role adding set True, for role removing False.
-                         If None, this function does nothing.
+        :param event: The BaseReactionEvent data
         """
-        if (payload.channel_id != Config().get(self)['message']['channel_id']
-                or payload.message_id != Config().get(self)['message']['message_id']
-                or role_add is None):
-            return
 
-        data = await ReactionEventData.convert(self.bot, payload)
-        emoji_str = str(data.emoji)
+        emoji_str = emoji.demojize(str(event.emoji), True)
         update_type = ""
         has_role_update = False
         for configured_role in self.rc():
             configured_emoji = self.rc()[configured_role][0]
             if configured_emoji == emoji_str:
                 role = discord.utils.get(self.bot.guild.roles, id=configured_role)
-                if role_add and role not in data.member.roles:
+                if isinstance(event, reactions.ReactionAddedEvent) and role not in event.member.roles:
                     update_type = "add"
-                    await RoleManagement.add_user_role(data.member, role)
+                    await RoleManagement.add_user_role(event.member, role)
                     has_role_update = True
-                elif not role_add and role in data.member.roles:
+                elif isinstance(event, reactions.ReactionRemovedEvent) and role in event.member.roles:
                     update_type = "remove"
-                    await RoleManagement.remove_user_role(data.member, role)
+                    await RoleManagement.remove_user_role(event.member, role)
                     has_role_update = True
 
         if has_role_update:
             await utils.log_to_admin_channel_without_ctx(self.bot,
                                                          **{'Type': "Self-assign role",
                                                             'Action': update_type,
-                                                            'User': data.member.mention,
-                                                            'Reaction': data.emoji,
+                                                            'User': event.member.mention,
+                                                            'Reaction': event.emoji,
                                                             'role': role.mention})
 
     @commands.group(name="role", invoke_without_command=True, help="Adds or removes the role to/from users roles",
@@ -286,13 +294,14 @@ class Plugin(BasePlugin, name="Role Management"):
 
         await utils.log_to_admin_channel(ctx)
 
-    @role.command(name="add", help="Creates a new role or updates its management data", usage="<role_name> [emoji|masterrole] [color]",
+    @role.command(name="add", help="Creates a new role or updates its management data",
+                  usage="<role_name> [emoji|masterrole] [color]",
                   description="Creates a new role on the server with the given management data."
-                               " If an emoji is given, the role will be self-assignable by the users via reaction in the role init message."
-                               " If a master role is given, a user with the master role can add/remove the role via !role <user> add <role>."
-                               " If the role already exists, it will be added to the role management with the given data."
-                               " If role is already in the role management, the management data will be updated."
-                               " As color a color name like 'blue' can be given or a hexcode like '#0000ff'.")
+                              " If an emoji is given, the role will be self-assignable by the users via reaction in the role init message."
+                              " If a master role is given, a user with the master role can add/remove the role via !role <user> add <role>."
+                              " If the role already exists, it will be added to the role management with the given data."
+                              " If role is already in the role management, the management data will be updated."
+                              " As color a color name like 'blue' can be given or a hexcode like '#0000ff'.")
     @commands.has_any_role(*Config().FULL_ACCESS_ROLES)
     async def role_add(self, ctx, role_name, emoji_or_masterrole, color: discord.Color = None):
         emoji_str = await utils.demojize(emoji_or_masterrole, ctx)
@@ -358,7 +367,7 @@ class Plugin(BasePlugin, name="Role Management"):
 
     @role.command(name="update", help="Reads the server data and updates the role management",
                   description="Updates the role management from server data and removes deleted roles from role management."
-                               " If a message content is given, this message will be used for the role management init message text.")
+                              " If a message content is given, this message will be used for the role management init message text.")
     @commands.has_any_role(*Config().FULL_ACCESS_ROLES)
     async def role_update(self, ctx, *message_content):
         if len(message_content) > 0:
