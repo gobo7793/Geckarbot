eda60a606dce47fef858088e02e375f1913248fa
Gerald Siegert <gerald.siegert@outlook.com>
2020-07-02 15:10:50 +0200

    add and removing dataset from ignorelist

--- subsystems/ignoring.py
+++ subsystems/ignoring.py
@@ -3,15 +3,19 @@ from datetime import datetime
 
 from conf import Config
 from botutils import utils
+from subsystems import timers
 
 
 ignoring_file_name = "ignoring"
 
 class IgnoreDataset:
-    """The ignoring dataset"""
+    """
+    The ignoring dataset.
+    Supports equal operation and checks equality based on user id, command name and channel id.
+    """
 
     def __init__(self, user: discord.User = None, command_name: str = "",
-                 channel: discord.TextChannel = None, until: datetime = datetime.max):
+                 channel: discord.TextChannel = None, until: datetime = datetime.max, job: timers.Job = None):
         """
         Creates a new ignoring dataset.
         Not needed args can be None, eg for ignoring user completely the args command_name and channel.
@@ -19,11 +23,30 @@ class IgnoreDataset:
         :param command_name: The command name to disabling for a user or completely in a channel
         :param channel: The channel in which a command is disabled
         :param until: The datetime on which the ignoring entry will be auto-removed
+        :param job: The timer subsystem job for auto-remove
         """
         self.user = user
         self.command_name = command_name
         self.channel = channel
         self.until = until
+        self.job = job
+
+    def __eq__(self, other):
+        if isinstance(other, IgnoreDataset):
+            user_res = IgnoreDataset._eq(self.user, other.user)
+            cmd_res = self.command_name == other.command_name
+            chan_res = IgnoreDataset._eq(self.channel, other.channel)
+
+            return user_res and cmd_res and chan_res
+        return  False
+
+    @classmethod
+    def _eq(cls, self, other):
+        """__eq__ helper method"""
+        if self is not None and other is not None:
+            return self == other
+        else:
+            return self is None and other is None
 
     def serialize(self):
         """
@@ -59,7 +82,8 @@ class IgnoreDataset:
 
     def to_raw_message(self):
         """
-        Builds an raw output message with the raw data
+        Builds an raw output message with the raw data.
+        Format: IgnoreDataset: User: user_name, Command: command_name, Channel: channel_name, Until: datetime
         :return: The raw message
         """
         return "IgnoreDataset: User: {}, Command: {}, Channel: {}, Until: {}".format(
@@ -112,7 +136,7 @@ class Ignoring:
         """Loads the ignorelist json"""
         jsondata = Config()._read_config_file(ignoring_file_name)
         for el in jsondata:
-            self.ignorelist.append(IgnoreDataset.deserialize(self.bot, el))
+            self.add(IgnoreDataset.deserialize(self.bot, el))
 
     def save(self):
         """Saves the current ignorelist to json"""
@@ -120,3 +144,48 @@ class Ignoring:
         for el in self.ignorelist:
             jsondata.append(el.serialize())
         Config()._write_config_file(ignoring_file_name, jsondata)
+
+    def add(self, dataset: IgnoreDataset):
+        """
+        Adds a IgnoreDataset to ignore list and schedules necessary timers for auto-remove
+        :param dataset: the dataset
+        :return: True if added, False if dataset already in list
+        """
+        if dataset in self.ignorelist:
+            return False
+
+        self.ignorelist.append(dataset)
+        if dataset.until < datetime.max:
+            timedict = timers.timedict(year=dataset.until.year, month=dataset.until.month,
+                                       monthday=dataset.until.day, hour=dataset.until.hour,
+                                       minute=dataset.until.minute)
+            job = self.bot.timers.schedule(self.auto_remove_callback, timedict, repeat=False)
+            job.data = dataset
+            dataset.job = job
+        self.save()
+        return True
+
+    async def auto_remove_callback(self, job):
+        """
+        The auto-remove callback method
+        :param job: the auto-remove job with the dataset
+        """
+        remove_result = self.remove(job.data)
+        msg = "Attempt auto-removing {}, Result: {}".format(job.data.to_raw_message(), remove_result)
+        await utils.write_admin_channel(self.bot, msg)
+
+    def remove(self, dataset: IgnoreDataset):
+        """
+        Removes a IgnoreDataset from ignore list and removes it's scheduled timer
+        :param dataset: the dataset
+        :return: True if dataset is removed, False if dataset not in list
+        """
+        if dataset not in self.ignorelist:
+            return False
+
+        self.ignorelist.remove(dataset)
+        if dataset.job is not None:
+            dataset.job.cancel()
+
+        self.save()
+        return True
