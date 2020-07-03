9ab3708085b4d8c501b8f5b3670807361d4a1c12
Gerald Siegert <gerald.siegert@outlook.com>
2020-07-03 23:38:29 +0200

    added adding methods

--- subsystems/ignoring.py
+++ subsystems/ignoring.py
@@ -17,7 +17,7 @@ class IgnoreType(enum.IntEnum):
     NA = 0
     """Not defined"""
     User = 1
-    """Blocks all interactions between user and bot. Disables command_name and channel arguments in IgnoreDataset."""
+    """Blocks any interactions between user and bot. Disables command_name and channel arguments in IgnoreDataset."""
     Command = 2
     """Disables command usage in specific channel. Disables user argument in IgnoreDataset."""
     User_Command = 3
@@ -36,6 +36,7 @@ class IgnoreDataset:
         Creates a new ignoring dataset.
         Not needed args can be None, eg for ignoring user completely the args command_name and channel.
         Disabled arguments based on type raises ValueError.
+
         :param ignore_type: The type of the ignore dataset, defines not possible arguments
         :param user: The user to ignore. For IgnoreType.User and IgnoreType.User_Command.
         :param command_name: The command name to disabling for a user or completely in a channel.
@@ -87,6 +88,8 @@ class IgnoreDataset:
 
     def serialize(self):
         """
+        Serializes the dataset to a dict
+
         :return: A dict with the keys user_id, command_name, channel_id, until
         """
         user_id = 0
@@ -108,6 +111,7 @@ class IgnoreDataset:
     def deserialize(cls, bot, d):
         """
         Constructs a IgnoreDataset object from a dict.
+
         :param bot: Geckarbot reference
         :param d: dict made by serialize()
         :return: IgnoreDataset object
@@ -121,7 +125,9 @@ class IgnoreDataset:
     def to_raw_message(self):
         """
         Builds an raw output message with the raw data.
+
         Format: IgnoreDataset: User: user_name, Command: command_name, Channel: channel_name, Until: datetime
+
         :return: The raw message
         """
         return "IgnoreDataset: Type: {}, User: {}, Command: {}, Channel: {}, Until: {}".format(
@@ -131,11 +137,13 @@ class IgnoreDataset:
     def to_message(self):
         """
         Builds an well formatted output message for listing the entries on ignore list.
+
         Format for User or User_Command IgnoreType:
             User user_name will be ignored [for command command_name] until.../forever.
         Format for Command IgnoreType:
             Command command_name is disabled in channel channel_name until.../forever.
         For other IgnoreTypes the raw message will be returned.
+
         :return: The well formatted message
         """
 
@@ -171,9 +179,7 @@ class Ignoring:
             self.load()
 
     def filter_ignore_list(self, ignore_type: IgnoreType):
-        """
-        Returns all ignore list entries of the given IgnoreType.
-        """
+        """Returns all ignore list entries of the given IgnoreType."""
         return [el for el in self.ignorelist if el.ignore_type == ignore_type]
 
     def load(self):
@@ -189,9 +195,14 @@ class Ignoring:
             jsondata.append(el.serialize())
         Config()._write_config_file(ignoring_file_name, jsondata)
 
+    #######
+    # Adding
+    #######
+
     def add(self, dataset: IgnoreDataset):
         """
         Adds a IgnoreDataset to ignore list and schedules necessary timers for auto-remove
+
         :param dataset: the dataset
         :return: True if added, False if dataset already in list
         """
@@ -209,18 +220,88 @@ class Ignoring:
         self.save()
         return True
 
-    async def auto_remove_callback(self, job):
+    def add_user(self, user: discord.User, until: datetime = datetime.max):
         """
-        The auto-remove callback method
-        :param job: the auto-remove job with the dataset
+        Adds the user to the ignore list to block all interactions from the user with the bot.
+
+        :param user: The user to block
+        :param until: The datetime to auto-remove the user from ignore list
+        :return: True if user was added, otherwise False if user already on list
         """
-        remove_result = self.remove(job.data)
-        msg = "Attempt auto-removing {}, Result: {}".format(job.data.to_raw_message(), remove_result)
-        await utils.write_admin_channel(self.bot, msg)
+        dataset = IgnoreDataset(IgnoreType.User, user=user, until=until)
+        return self.add(dataset)
+
+    def add_user_id(self, user_id: int, until: datetime = datetime.max):
+        """
+        Adds the user to the ignore list to block all interactions from the user with the bot.
+
+        :param user_id: The id of the user to block
+        :param until: The datetime to auto-remove the user from ignore list
+        :return: True if user was added, otherwise False if user already on list
+        """
+        user = self.bot.get_user(user_id)
+        return self.add_user(user, datetime)
+
+    def add_command(self, command_name: str, channel: discord.TextChannel, until: datetime = datetime.max):
+        """
+        Adds the command in the ignore list to disable the command in the specific channel.
+
+        :param command_name: The full qualified command name (eg. 'dsc set')
+        :param channel: the channel in which the command will be disabled
+        :param until: The datetime to auto-remove the command from ignore list
+        :return: True if command was added, otherwise False if command in channel already on list
+        """
+        dataset = IgnoreDataset(IgnoreType.Command, command_name=command_name, channel=channel, until=until)
+        return self.add(dataset)
+
+    def add_command_id(self, command_name: str, channel_id: int, until: datetime = datetime.max):
+        """
+        Adds the command in the ignore list to disable the command in the specific channel.
+
+        :param command_name: The full qualified command name (eg. 'dsc set')
+        :param channel_id: the channel id in which the command will be disabled
+        :param until: The datetime to auto-remove the command from ignore list
+        :return: True if command was added, otherwise False if command in channel already on list
+        """
+        channel = self.bot.get_channel(channel_id)
+        return self.add_command(command_name, channel, until)
+
+    def add_user_command(self, user: discord.User, command_name: str, until: datetime = datetime.max):
+        """
+        Adds the user and command to ignore list to block any interactions of the user with the specific command.
+
+        :param user: The user to block
+        :param command_name: The command to block for the user, Should be, but not necessarily, the full qualified
+            command name. Depending on the checking implementation for the specific command.
+        :param until: The datetime to auto-remove the command from ignore list
+        :return: True if user and command was added,
+            otherwise False if command interactions already blocked for the user
+        """
+        dataset = IgnoreDataset(IgnoreType.User_Command, user=user, command_name=command_name, until=until)
+        return self.add(dataset)
+
+    def add_user_id_command(self, user_id: int, command_name: str, until: datetime = datetime.max):
+        """
+        Adds the user and command to ignore list to block any interactions of the user with the specific command.
+
+        :param user_id: The user id to block
+        :param command_name: The command to block for the user, Should be, but not necessarily, the full qualified
+            command name. Depending on the checking implementation for the specific command.
+        :param until: The datetime to auto-remove the command from ignore list
+        :return: True if user and command was added,
+            otherwise False if command interactions already blocked for the user
+        """
+        user = self.bot.get_user(user_id)
+        return self.add_user_command(user, command_name, until)
+
+    #######
+    # Removing
+    #######
 
     def remove(self, dataset: IgnoreDataset):
         """
         Removes a IgnoreDataset from ignore list and removes it's scheduled timer
+
         :param dataset: the dataset
         :return: True if dataset is removed, False if dataset not in list
         """
@@ -234,9 +315,24 @@ class Ignoring:
         self.save()
         return True
 
+    async def auto_remove_callback(self, job):
+        """
+        The auto-remove callback method
+
+        :param job: the auto-remove job with the dataset
+        """
+        remove_result = self.remove(job.data)
+        msg = "Attempt auto-removing {}, Result: {}".format(job.data.to_raw_message(), remove_result)
+        await utils.write_admin_channel(self.bot, msg)
+
+    #######
+    # Checking
+    #######
+
     def check_user_id(self, user_id: int):
         """
         Checks if all bot interaction with user should be blocked
+
         :param user_id: the user id
         :return: True if user interactions should be blocked, otherwise False
         """
@@ -248,6 +344,7 @@ class Ignoring:
     def check_user(self, user: discord.User):
         """
         Checks if all bot interaction with user should be blocked
+
         :param user: the user
         :return: True if user interactions should be blocked, otherwise False
         """
@@ -257,6 +354,7 @@ class Ignoring:
         """
         Checks if the context is invoked by a command which is on the ignore list
         for the channel in which the command was called.
+
         :param ctx: the command context
         :return: True if command is blocked in channel, otherwise False
         """
@@ -269,6 +367,7 @@ class Ignoring:
     def check_user_id_command(self, user_id: int, command_name: str):
         """
         Checks if the user is blocked for all interactions with the command.
+
         :param user_id: the user id
         :param command_name: the command name
         :return: True if user is blocked for command, otherwise False
@@ -281,6 +380,7 @@ class Ignoring:
     def check_user_command(self, user: discord.User, command_name: str):
         """
         Checks if the user is blocked for all interactions with the command.
+
         :param user: the user
         :param command_name: the command name
         :return: True if user is blocked for command, otherwise False
