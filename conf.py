24d7067c2e0c3c3bdc93fd3ef4e9eaca24532549
Gerald Siegert <gerald.siegert@outlook.com>
2020-07-27 13:57:31 +0200

    fixed empty comment at line end

--- conf.py
+++ conf.py
@@ -11,7 +11,7 @@ class _Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]#
+        return cls._instances[cls]
 
 
 class Configurable:
