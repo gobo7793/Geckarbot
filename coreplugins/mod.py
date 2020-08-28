ea693454cd22c1a58e837e9e285d371671bbd36b
costamiri <dennisdelacosta@gmail.com>
2020-08-28 16:02:08 +0200

    disable only existing commands

--- coreplugins/mod.py
+++ coreplugins/mod.py
@@ -5,8 +5,8 @@ import pkgutil
 from discord.ext import commands
 
 import botutils.parsers
-from conf import Config, Lang
-from botutils import utils, permchecks
+from conf import Config, Lang, Storage
+from botutils import utils, permchecks, converters
 from botutils.stringutils import paginate
 from botutils.converters import get_best_username
 from base import BasePlugin, ConfigurableType
@@ -128,6 +128,11 @@ class Plugin(BasePlugin, name="Bot Management Commands"):
                                 "If a user uses a command which is blocked for the user, "
                                 "the bot doesn't response anything, like the command wouldn't exists.")
     async def disable(self, ctx, command, *args):
+        customcmds = Storage.get(converters.get_plugin_by_name(self.bot, "customcmd"))
+        if command not in self.bot.all_commands and command not in customcmds:
+            await ctx.message.add_reaction(Lang.CMDERROR)
+            await ctx.send(Lang.lang(self, 'cmd_not_found', command))
+            return
         if command == "enable":
             await ctx.message.add_reaction(Lang.CMDERROR)
             await ctx.send(Lang.lang(self, 'enable_cant_blocked'))
