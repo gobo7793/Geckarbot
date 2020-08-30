21e691bd6ad7b708501b7a27d8637d9c23c12709
Gerald Siegert <gerald.siegert@outlook.com>
2020-08-30 14:50:18 +0200

    better member/user search for dsc host

--- plugins/dsc.py
+++ plugins/dsc.py
@@ -126,7 +126,9 @@ class Plugin(BasePlugin, name="Discord Song Contest"):
             await ctx.send(Lang.lang(self, 'must_set_host'))
             return
 
-        host_nick = discord.utils.get(self.bot.guild.members, id=Storage.get(self)['host_id']).mention
+        host_nick = self.bot.guild.get_member(Storage.get(self)['host_id'])
+        if host_nick is None:
+            host_nick = self.bot.get_user(Storage.get(self)['host_id'])
 
         embed = discord.Embed()
         embed.add_field(name=Lang.lang(self, 'current_host'), value=host_nick)
