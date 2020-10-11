4772bff09963a2336dff88d523bdd7698c076e8a
costamiri <dennisdelacosta@gmail.com>
2020-10-11 15:55:32 +0200

    Sport: !buli special cases * help

--- plugins/sport.py
+++ plugins/sport.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 
 from base import BasePlugin
 from botutils import restclient
+from conf import Lang
 
 
 class Plugin(BasePlugin, name="Sport"):
@@ -16,14 +17,25 @@ class Plugin(BasePlugin, name="Sport"):
         self.can_reload = True
 
     def default_config(self):
-        return {}
+        return {
+            'leagues': ["bl1", "bl2", "bl3", "uefanl"]
+        }
 
     def default_storage(self):
         return []
 
+    def command_help_string(self, command):
+        return Lang.lang(self, "help_{}".format("_".join(command.qualified_name.split())))
+
+    def command_description(self, command):
+        name = "_".join(command.qualified_name.split())
+        lang_name = "description_{}".format(name)
+        result = Lang.lang(self, lang_name)
+        return result if result != lang_name else Lang.lang(self, "help_{}".format(name))
+
     @commands.command(name="buli")
     async def buli_livestaende(self, ctx, allmatches=None):
-        matches = restclient.Client("https://www.openligadb.de/api").make_request("/getmatchdata/bl1")
+        matches = restclient.Client("https://www.openligadb.de/api").make_request("/getmatchdata/bl3")
         finished, running, upcoming = [], [], []
         for match in matches:
             if match.get('MatchIsFinished', False):
@@ -43,13 +55,19 @@ class Plugin(BasePlugin, name="Sport"):
             team_h = m.get('Team1', {}).get('TeamName')
             team_a = m.get('Team2', {}).get('TeamName')
             goals = m.get('Goals', [])
-            goals_h = max(0, *(x.get('ScoreTeam1', 0) for x in goals)) if len(goals) else 0
-            goals_a = max(0, *(x.get('ScoreTeam2', 0) for x in goals)) if len(goals) else 0
+            goals_h = max(0, *(x.get('ScoreTeam1', 0) for x in goals)) if len(goals) else ("–" if m in upcoming else 0)
+            goals_a = max(0, *(x.get('ScoreTeam2', 0) for x in goals)) if len(goals) else ("–" if m in upcoming else 0)
             return "{} [{}:{}] {}".format(team_h, goals_h, goals_a, team_a)
 
-        embed = discord.Embed(title="Bundesliga - Live")
-        embed.description = "\n".join(match_msg(m) for m in running)
-        if allmatches == "all":
-            embed.add_field(name="Beendet", value="\n".join(match_msg(m) for m in finished), inline=False)
-            embed.add_field(name="Bevorstehend", value="\n".join(match_msg(m) for m in upcoming), inline=False)
+        embed = discord.Embed(title=Lang.lang(self, 'buli_title'))
+        running_msg = "\n".join(match_msg(m) for m in running)
+        if running_msg:
+            embed.description = "\n".join(match_msg(m) for m in running)
+        if allmatches == "all" or not running_msg:
+            finished_msg = "\n".join(match_msg(m) for m in finished)
+            upcoming_msg = "\n".join(match_msg(m) for m in upcoming)
+            if finished_msg:
+                embed.add_field(name=Lang.lang(self, 'match_finished'), value=finished_msg, inline=False)
+            if upcoming_msg:
+                embed.add_field(name=Lang.lang(self, 'match_upcoming'), value=upcoming_msg, inline=False)
         await ctx.send(embed=embed)
