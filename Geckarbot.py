edc2c24c543ccbd037051fb09aa1cc40e74f0bdf
Christian Krüger <christian@emailgsm.de>
2020-10-18 19:10:35 +0200

    Update Geckarbot.py

--- Geckarbot.py
+++ Geckarbot.py
@@ -36,7 +36,7 @@ class Geckarbot(commands.Bot):
     Basic bot info
     """
     NAME = "Geckarbot"
-    VERSION = "2.4.0"
+    VERSION = "2.5.0"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
     CONFIG_DIR = "config"
