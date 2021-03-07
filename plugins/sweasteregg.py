90229a89197026ea7d18e52f980477ca1f57f21c
Gerald Siegert <gerald.siegert@outlook.com>
2021-03-07 18:08:36 +0100

    more memes and timer callbacks

--- plugins/sweasteregg.py
+++ plugins/sweasteregg.py
@@ -1,10 +1,11 @@
-from datetime import datetime, date, timedelta
+import asyncio
+from datetime import date, timedelta
 from typing import List, Optional
 
 from base import BasePlugin
-from conf import Config
-from subsystems.timers import Job, timedict
+from conf import Config, Storage
 from subsystems.presence import PresenceMessage, PresencePriority
+from subsystems.timers import Job, timedict
 
 
 class Plugin(BasePlugin):
@@ -17,6 +18,11 @@ class Plugin(BasePlugin):
         self.meme_timer = None  # type: Optional[Job]
         self.channel = self.bot.guild.get_channel(Config.get(self)["channel_id"])
 
+        if Config.get(self)["is_running"]:
+            asyncio.run_coroutine_threadsafe(self._start(), self.bot.loop)
+        else:
+            self._prepare()
+
     def default_config(self):
         return {
             "version": 1,
@@ -26,37 +32,38 @@ class Plugin(BasePlugin):
             "monthday": 4,
             # ints of storage IDs of the memes
             "meme_order": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-            "last_meme_index": -1  # index of the last showed meme in meme_order
+            "last_meme_index": -1,  # index of the last showed meme in meme_order
+            "is_running": False
         }
 
     def default_storage(self):
         return {
-            "memes": [
-                "https://tenor.com/view/hello-there-gif-9442662",
-                "https://tenor.com/view/general-kenobi-kenobi-general-hello-there-star-wars-gif-13723705",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "https://i.redd.it/u4viabqenpi61.png",
-                "https://i.redd.it/zf3b2i3y4hi61.jpg",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "https://tenor.com/view/your-move-obi-wan-kenobi-star-wars-jedi-master-gif-15824683",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "https://i.redd.it/aozydy26hki61.jpg",
-                "https://tenor.com/view/general-grievous-abandon-ship-funny-abort-evacuate-gif-10721574",
-            ]
+            "memes": {
+                0: "https://tenor.com/view/day-long-remembered-darth-vader-grand-moff-tarkin-tarkin-vader-gif-16214689",
+                1: "https://tenor.com/view/hello-there-gif-9442662",
+                2: "https://tenor.com/view/general-kenobi-kenobi-general-hello-there-star-wars-gif-13723705",
+                3: "https://tenor.com/view/your-move-obi-wan-kenobi-star-wars-jedi-master-gif-15824683",
+                4: "https://i.redd.it/aozydy26hki61.jpg",
+                5: "https://cdn.discordapp.com/attachments/706129811382337566/818086156679118858/FB_IMG_1615117281039.jpg",
+                6: "",
+                7: "https://i.redd.it/84gafcvrjcl61.png",
+                8: "",
+                9: "https://i.redd.it/zf3b2i3y4hi61.jpg",
+                10: "",
+                11: "",
+                12: "",
+                13: "https://i.redd.it/003i3tinfel61.jpg",
+                14: "",
+                15: "",
+                16: "https://tenor.com/view/impeachment-love-democracy-ilove-democracy-gif-15723806",
+                17: "",
+                18: "",
+                19: "",
+                20: "",
+                21: "https://i.redd.it/a520dfy1bbl61.png",
+                22: "",
+                23: "https://tenor.com/view/general-grievous-abandon-ship-funny-abort-evacuate-gif-10721574",
+            }
         }
 
     def get_lang(self):
@@ -70,7 +77,8 @@ class Plugin(BasePlugin):
                     "the Imperial March",
                     "with the Cantina Band",
                     "with his Death Star",
-                    "with the Force"
+                    "with the Force",
+                    "Emperor"
                 ]
             },
             "de": {
@@ -82,39 +90,52 @@ class Plugin(BasePlugin):
                     "den Imperial March",
                     "mit der Cantina Band",
                     "mit seinem Todesstern",
-                    "mit der Macht"
+                    "mit der Macht",
+                    "Imperator"
                 ]
             }
         }
 
-    def _prepare_easteregg(self):
+    def _prepare(self):
         """Prepares the easteregg"""
-        pass
+        month = Config.get(self)["month"]
+        monthday = Config.get(self)["monthday"]
+        start_date = date(year=date.today().year, month=month, day=monthday) - timedelta(days=1)
+        otd = timedict(year=start_date.year, month=start_date.month, monthday=start_date.day,
+                       hour=23, minute=30)
+        self.orga_timer = self.bot.timers.schedule(self._stop, otd, repeat=False)
+        Config.get(self)["last_meme_index"] = -1
 
-    async def _start_easteregg(self, job):
+    async def _start(self, job=None):
         """Starts the easteregg"""
-        month = int(Config.get(self)["month"])
-        monthday = int(Config.get(self)["monthday"])
+        month = Config.get(self)["month"]
+        monthday = Config.get(self)["monthday"]
 
         presence_strings = self.get_lang().get(self.bot.LANGUAGE_CODE, "en")["presences"]
         for presence_str in presence_strings:
             self.presences.append(self.bot.presence.register(presence_str, PresencePriority.HIGH))
-        mtd = timedict(month=month, monthday=monthday,
+        mtd = timedict(year=date.today().year, month=month, monthday=monthday,
                        minute=[i for i in range(0, 60, Config.get(self)["mtimer_min"])])
         self.meme_timer = self.bot.timers.schedule(self._mtimer_callback, mtd, repeat=True)
 
         finish_date = date(year=date.today().year, month=month, day=monthday) + timedelta(days=1)
         otd = timedict(year=finish_date.year, month=finish_date.month, monthday=finish_date.day)
-        self.orga_timer = self.bot.timers.schedule(self._stop_easteregg, otd, repeat=False)
+        self.orga_timer = self.bot.timers.schedule(self._stop, otd, repeat=False)
 
-    async def _stop_easteregg(self, job):
+        Config.get(self)["is_running"] = True
+        Config.save(self)
+
+    async def _stop(self, job):
         """Stops the easteregg"""
         for presence in self.presences:
             presence.deregister()
-        self.meme_timer.cancel()
+
+        Config.get(self)["is_running"] = False
+        Config.save(self)
 
     async def _mtimer_callback(self, job):
         """The callback for the meme_timer"""
         if self.channel is None:
             return
-
+        Config.get(self)["last_meme_index"] += 1
+        self.channel.send(Storage.get(self)["memes"][Config.get(self)["last_meme_index"]])
