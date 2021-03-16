74826a65aa017e810a24a767477e13d10ed749a8
Christian Krüger <christian@emailgsm.de>
2021-03-16 12:43:18 +0100

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -40,7 +28,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tools', '/test', 'config', 'lang']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/generate.sh
+++ docs/generate.sh
@@ -2,7 +2,9 @@
 #pip3 install sphinx
 
 # refresh source files
+mv index.rst index.rst_
 rm *.rst
+mv index.rst_ index.rst
 sphinx-apidoc -o . ..
 
 # Build
