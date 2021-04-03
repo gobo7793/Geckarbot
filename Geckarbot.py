6c122a74575d095416ab450dc3506f502821e3f8
Gerald Siegert <gobo7793@users.noreply.github.com>
2021-04-03 15:28:42 +0200

    Version 2.9.2

--- Geckarbot.py
+++ Geckarbot.py
@@ -39,7 +39,7 @@ class Geckarbot(commands.Bot):
     Basic bot info
     """
     NAME = "Geckarbot"
-    VERSION = "2.9.1"
+    VERSION = "2.9.2"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
     CONFIG_DIR = "config"
