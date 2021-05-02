642356ac7a3d7a381b1c67758778bd4b75a81983
costamiri <dennisdelacosta@gmail.com>
2021-05-02 12:54:04 +0200

    Update Geckarbot.py

--- Geckarbot.py
+++ Geckarbot.py
@@ -42,7 +42,7 @@ class Geckarbot(commands.Bot):
     Basic bot info
     """
     NAME = "Geckarbot"
-    VERSION = "2.10.1"
+    VERSION = "2.10.2"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
     CONFIG_DIR = "config"
