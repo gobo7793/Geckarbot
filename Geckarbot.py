66d052602378ff4a75cdb7fc8f34729e08085d62
costamiri <dennisdelacosta@gmail.com>
2021-06-19 17:57:59 +0200

    Release 2.11.0

--- Geckarbot.py
+++ Geckarbot.py
@@ -42,7 +42,7 @@ class Geckarbot(commands.Bot):
     Basic bot info
     """
     NAME = "Geckarbot"
-    VERSION = "2.10.2"
+    VERSION = "2.11.0"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
     CONFIG_DIR = "config"
