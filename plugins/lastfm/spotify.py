4a7a8a86973545abc8c7134a4085f807bf26cddf
Christian Krüger <christian@emailgsm.de>
2021-08-19 23:54:36 +0200

    linting

--- plugins/lastfm/spotify.py
+++ plugins/lastfm/spotify.py
@@ -14,18 +14,30 @@ LAYERMAP = {
 
 
 class NoCredentials(Exception):
+    """
+    Raised when a request is made without API authorization credentials being set.
+    """
     pass
 
 
 class AuthError(Exception):
+    """
+    Raised when authorization is not successful.
+    """
     pass
 
 
 class ApiError(Exception):
+    """
+    Raised when the API returns an error.
+    """
     pass
 
 
 class Client:
+    """
+    Implements the Spotify API.
+    """
     def __init__(self, plugin):
         self.plugin = plugin
         self._client_id = None
@@ -48,12 +60,23 @@ class Client:
         return self._client_id, self._client_secret
 
     async def set_credentials(self, client_id, client_secret):
+        """
+        Sets client ID and client secret for authorization with the API.
+
+        :param client_id: Client ID
+        :param client_secret: Client secret
+        """
         self._client_id = client_id
         self._client_secret = client_secret
         if client_id is not None and client_secret is not None:
             await self.auth()
 
     async def auth(self):
+        """
+        Refreshes the authorization token. Client ID and client secret need to be set for this.
+
+        :raises AuthError: If authorization does not work (i.e. no token is returned by the auth api)
+        """
         self.logger.debug("Refreshing authorization token")
         self.auth_client.auth_basic(*self.credentials)
         data = "grant_type=client_credentials"
@@ -96,6 +119,14 @@ class Client:
         return r
 
     async def search(self, searchstring, layer=Layer.TITLE):
+        """
+        Implements Spotify's search API. Submits a search string and returns the first element found in the specified
+        layer.
+
+        :param searchstring: Search string
+        :param layer: layer that is to be searched in
+        :return: First song that is found with searchstring
+        """
         params = {
             "q": searchstring,
             "type": LAYERMAP[layer],
@@ -106,7 +137,7 @@ class Client:
 
     async def enrich_song(self, song: Song):
         """
-        Adds spotify links to a song.
+        Adds spotify links to a song by fetching them from the API.
 
         :param song: Song object to be enriched
         """
