6a2aaf413c4d3057340f42da1cf40a6ace35b70f
costamiri <dennisdelacosta@gmail.com>
2021-08-26 17:44:32 +0200

    test get

--- test/test_sheetsapi.py
+++ test/test_sheetsapi.py
@@ -15,18 +15,13 @@ class SheetsApiTestCase(unittest.TestCase):
 
     def test_get(self):
         """Tests getting cell data from sheet"""
-        # only for current state of sheets as of 2020-07-18 12:00 pm
-        c = sheetsclient.Client(self.bot, "1mSDrTqdcOSOuvR9Y9hMzuEx1X9puaUVuqs7yp6Ju6_M")
-        v = c.get("Archiv!C6")
-        self.assertEqual(v[0][0], "Chr1s")
-
-        c = sheetsclient.Client(self.bot, "1HH42s5DX4FbuEeJPdm8l1TK70o2_EKADNOLkhu5qRa8")
-        v = c.get("B4")
-        self.assertEqual(v[0][0], "User")
-
-        c = sheetsclient.Client(self.bot, "1HH42s5DX4FbuEeJPdm8l1TK70o2_EKADNOLkhu5qRa8")
-        v = c.get("Hall of Fame!B4:D13")
-        self.assertEqual(v[4][1], "Splitt13")
+        c = sheetsclient.Client(self.bot, "1rANJqqXmWiQ4CCfcc695uKBbsjqRpeghHw-AYDDkKM0")
+        v = c.get("C3")
+        self.assertEqual(v[0][0], "Hello, World")
+        v = c.get("B3:B4")
+        self.assertEqual(v, [["Hello"], ["World"]])
+        v = c.get("Test1!C4")
+        self.assertEqual(v[0][0], 5)
 
 
 if __name__ == '__main__':
