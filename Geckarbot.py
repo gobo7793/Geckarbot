109d368a36cf787bc53f94269117e3d4cb4d3cf8
Christian Krüger <christian@emailgsm.de>
2021-08-28 13:09:57 +0200

    Version bump

--- Geckarbot.py
+++ Geckarbot.py
@@ -30,7 +30,7 @@ class Geckarbot(commands.Bot):
     Basic bot info
     """
     NAME = "Geckarbot"
-    VERSION = "2.13.5"
+    VERSION = "2.13.6"
     PLUGIN_DIR = "plugins"
     CORE_PLUGIN_DIR = "coreplugins"
     CONFIG_DIR = "config"
