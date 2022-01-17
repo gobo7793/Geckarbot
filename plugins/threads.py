dc247e454a5e51db3e16b4661638e458f761dd84
costamiri <dennisdelacosta@gmail.com>
2022-01-17 22:11:15 +0100

    thread pin

--- plugins/threads.py
+++ plugins/threads.py
@@ -4,6 +4,7 @@ from nextcord.ext.commands import Context
 
 from base.configurable import BasePlugin
 from base.data import Config, Lang
+from botutils.utils import add_reaction
 from services.helpsys import DefaultCategories
 
 
@@ -13,6 +14,17 @@ class Plugin(BasePlugin, name="Threads"):
         super().__init__()
         self.bot = Config().bot
         self.bot.register(self, DefaultCategories.MISC)
+
+    async def limit_to_bot_threads(self, ctx: Context) -> bool:
+        if not isinstance(ctx.channel, Thread):
+            await ctx.send(Lang.lang(self, "channel_not_thread"))
+            await add_reaction(ctx.message, Lang.CMDERROR)
+            return False
+        if ctx.channel.owner != self.bot.user:
+            await ctx.send(Lang.lang(self, "thread_not_owned"))
+            await add_reaction(ctx.message, Lang.CMDERROR)
+            return False
+        return True
 
     @commands.group(name="thread")
     async def cmd_thread(self, ctx):
@@ -27,12 +39,26 @@ class Plugin(BasePlugin, name="Threads"):
         if not isinstance(channel, TextChannel):
             await ctx.send(Lang.lang(self, "open_unable"))
             return
-        thread = await ctx.channel.create_thread(name=title, type=ChannelType.public_thread)
+        thread = await channel.create_thread(name=title, type=ChannelType.public_thread)
         await thread.send("👋")
         await thread.add_user(ctx.author)
 
     @cmd_thread.command(name="archive")
     async def cmd_thread_delete(self, ctx: Context):
-        if not isinstance(ctx.channel, Thread) or ctx.channel.owner != self.bot.user:
+        if not self.limit_to_bot_threads(ctx):
             return
         await ctx.channel.edit(archived=True)
+
+    @cmd_thread.command(name="pin")
+    async def cmd_thread_pin(self, ctx: Context):
+        if not await self.limit_to_bot_threads(ctx):
+            return
+        if not ctx.message.reference:
+            await ctx.send(Lang.lang(self, "pin_no_reply"))
+            await add_reaction(ctx.message, Lang.CMDERROR)
+            return
+        if not ctx.message.reference.resolved:
+            await ctx.send(Lang.lang(self, "pin_err"))
+            await add_reaction(ctx.message, Lang.CMDERROR)
+            return
+        await ctx.message.reference.resolved.pin()
