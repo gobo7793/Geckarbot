366aeb902433a1aba011b2b4860f76dd88a28042
Christian Krüger <christian@emailgsm.de>
2022-05-06 15:23:23 +0200

    cmd random

--- plugins/customcmd.py
+++ plugins/customcmd.py
@@ -1240,3 +1240,36 @@ class Plugin(BasePlugin, name="Custom CMDs"):
         c.clear_aliases()
         self._save()
         await utils.add_reaction(ctx.message, Lang.CMDSUCCESS)
+
+    @cmd.command(name="random", aliases=["rnd", "rng"])
+    async def cmd_random(self, ctx, *args):
+        candidates = []
+        weights = []
+        for el in self.commands.values():
+            if isinstance(el, TextCmd):
+                candidates.append(el)
+                weights.append(len(el.texts))
+
+        if not candidates:
+            await add_reaction(ctx.message, Lang.CMDNOCHANGE)
+            return
+
+        cmd = random.choices(candidates, weights=weights)[0]
+        await cmd.invoke(ctx.message, *args)
+
+    @cmd.command(name="image", aliases=["randomimage"])
+    async def cmd_image(self, ctx):
+        candidates = []
+        for el in self.commands.values():
+            if not isinstance(el, TextCmd):
+                continue
+
+            for text in el.texts:
+                if text.startswith("http"):
+                    candidates.append(text)
+
+        if not candidates:
+            await add_reaction(ctx.message, Lang.CMDNOCHANGE)
+            return
+
+        await ctx.send(random.choice(candidates))
